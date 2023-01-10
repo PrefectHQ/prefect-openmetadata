b34d1c371b56ea61d5fb53933ac76c7114216459
Anna Geller <anna@prefect.io>
2023-01-10 06:55:38 +0100

    flake8

--- docs/example_postgres_ingestion.py
+++ docs/example_postgres_ingestion.py
@@ -1,6 +1,7 @@
 """
 pip install -r requirements.txt --no-deps
-Login to the UI with user admin, password admin and use the link below to see how to generate jwtToken:
+Login to the UI with user admin, password admin
+and use the link below to see how to generate jwtToken:
 https://docs.open-metadata.org/how-to-guides/cli-ingestion-with-basic-auth
 """
 from prefect_openmetadata.flows import ingest_metadata
@@ -29,7 +30,7 @@ workflowConfig:
     authProvider: openmetadata
     securityConfig:
       jwtToken: 'eyJraWQiOiJHYjM4OWEtOWY3Ni1nZGpzLWE5MmotMDI0MmJrOTQzNTYiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJpbmdlc3Rpb24tYm90IiwiaXNCb3QiOnRydWUsImlzcyI6Im9wZW4tbWV0YWRhdGEub3JnIiwiaWF0IjoxNjczMjI4MjEwLCJlbWFpbCI6ImluZ2VzdGlvbi1ib3RAb3Blbm1ldGFkYXRhLm9yZyJ9.fkEZAWUbQEWLC19tOMTMlTxZhkY4YUdOAzDucQVtTtvrgAn8Q3Oi8Ogxc12lCUZh7VI8ykxXFhYqjQbTjp00ZFrSt4hoDn5j7dQ9Ewz4bv35I9m931JkoX-aHclzFeqhSlI6kqkS5KL7sDKhT9eVFEGVmNoBVpcSnz2_6PUD8h0LeUQOxYG4_frqungSWunVScshpqEG-mCyAeXnIFbbt0Wu6x8w-zuNfyEiI3K5VWf2NejEG1hWGYBQjVJNTu7ZQyPdxTYBxkK7kFuj-sN3ASPzdX0DchnUpGpRXpzN8QB73pE9xeLJhD_NB7jPOu1b59XCpdg47UGooRjpumNbuQ'
-"""
+"""  # noqa
 
 if __name__ == "__main__":
     ingest_metadata(postgres)
